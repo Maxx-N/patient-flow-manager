e52439cc31fad5f421a108ccab45501a9e6ac327
maximenorval <maximenorval@gmail.com>
2022-09-06 18:49:58 +0200

    creating an environment variable api_url and calling fetchCards from the board component

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CardService } from 'src/app/services/card.service';
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
-
-  constructor() { }
+  constructor(private cardService: CardService) {}
 
   ngOnInit(): void {
+    this.cardService.fetchCards();
   }
-
 }
--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,14 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root',
 })
 export class CardService {
+  private readonly CARDS_API_URL = `${environment.api_url}/cards`;
+
   constructor(private http: HttpClient) {}
 
   fetchCards(): void {
-    this.http.get('http://localhost:3000/cards').subscribe((res) => {
+    this.http.get(this.CARDS_API_URL).subscribe((res) => {
       console.log(res);
     });
   }
